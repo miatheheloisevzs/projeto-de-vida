12a9f749da1c3f8f0746dc984c23fe3590a5b301
maithe heloise <maithe.faria@escola.pr.gov.br>
2026-05-11 10:02:57 -0300

    salvar

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;1 >botoes.length;i++){
- botoes                                                                 [[]]
+ botoes[i].onclick = function(){
+
+    for(let j=0;j<botoes.length;j++){
+        botoes[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+ }                                                           [[]]
  
 }
